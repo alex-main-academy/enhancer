3df9dce22b44b8807197d7c5ccc4b02228551302
alex-main-academy <oleksandrilnytskyi@gmail.com>
2024-01-31 09:34:59 +0200

    add-pricing-page

--- js/main.js
+++ js/main.js
@@ -25,5 +25,70 @@ openMenuBtn.addEventListener("click", () => {
 
 // work with twenty twenty library
 $(document).ready(function () {
-    $(".before-after-container").twentytwenty();
+    if ($.fn.twentytwenty) {
+        $(".before-after-container").twentytwenty();
+    } else {
+        return;
+    }
 });
+
+// work with change pricing toggle
+const changePricingBtn =
+    document.querySelector(".js-choose-pricing-toggle") || null;
+let toggle = 0;
+
+if (changePricingBtn) {
+    changePricingBtn.addEventListener("click", () => {
+        toggle = !toggle;
+
+        const oneMonthText = document.querySelector(".js-one-month");
+        const threeMonthText = document.querySelector(".js-three-month");
+        const oneMonthPricing = document.querySelector(
+            ".js-plan-list-one-month"
+        );
+        const threeMonthPricing = document.querySelector(
+            ".js-plan-list-three-month"
+        );
+
+        if (toggle) {
+            changePricingBtn.classList.add("active");
+            oneMonthText.classList.remove("active");
+            threeMonthText.classList.add("active");
+            oneMonthPricing.classList.remove("active");
+            threeMonthPricing.classList.add("active");
+        } else {
+            changePricingBtn.classList.remove("active");
+            threeMonthText.classList.remove("active");
+            oneMonthText.classList.add("active");
+            threeMonthPricing.classList.remove("active");
+            oneMonthPricing.classList.add("active");
+        }
+    });
+}
+
+// work with faq
+const faqSection = document.querySelector(".js-faq") || null;
+
+if (faqSection) {
+    const faqItem = document.querySelectorAll(".js-faq-item");
+
+    faqItem.forEach((elem) => {
+        let faqToggle = 0;
+        elem.addEventListener("click", (event) => {
+            const clickedElement = event.target;
+            const currentItem = event.currentTarget;
+
+            if (clickedElement.classList.contains("js-open-faq-answer")) {
+                faqToggle = !faqToggle;
+
+                if (faqToggle) {
+                    currentItem.classList.add("active");
+                    clickedElement.classList.add("active");
+                } else {
+                    currentItem.classList.remove("active");
+                    clickedElement.classList.remove("active");
+                }
+            }
+        });
+    });
+}
